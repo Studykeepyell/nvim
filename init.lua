e37f1bea0fa1d0bc56fb357f38e4e875e903c869
StudyKeepYell <skyjin6667@gmail.com>
2025-06-14 23:19:11 -0400

    Initial commit

---
+++ init.lua
@@ -0,0 +1,59 @@
+require("core.options")
+require("core.keymaps")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        error("Error cloning lazy.nvim:\n" .. out)
+    end
+end
+
+---@type vim.Option
+local rtp = vim.opt.rtp
+rtp:prepend(lazypath)
+
+-- [[ Configure and install plugins ]]
+--
+--  To check the current status of your plugins, run
+--    :Lazy
+--
+--  You can press `?` in this menu for help. Use `:q` to close the window
+--
+--  To update plugins you can run
+--    :Lazy update
+--
+-- NOTE: Here is where you install your plugins.
+require("lazy").setup({
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+            -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+        },
+        lazy = false, -- neo-tree will lazily load itself
+        ---@module "neo-tree"
+        ---@type neotree.Config?
+        opts = {
+            -- fill any relevant options here
+        },
+    },
+    -- Lazy
+    require("plugins.colortheme"),
+    require("plugins.neotree"),
+    require("plugins.bufferline"),
+    require("plugins.lualine"),
+    require("plugins.treesitter"),
+    require("plugins.telescope"),
+    require("plugins.lsp"),
+    require("plugins.autocompletion"),
+    require("plugins.autoformatting"),
+    require("plugins.gitsigns"),
+    require("plugins.alpha"),
+    require("plugins.indent-blankline"),
+    require("plugins.misc"),
+})
